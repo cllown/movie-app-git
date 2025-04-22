7b40db4075468c971a87af8673d1a07100a382b6
cllown <artoorclownxd@gmail.com>
2025-04-22 13:01:04 +0300

    Auto-generated commit

---
+++ chunk-A5SU6ARD.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-NVV6CR6E.js";import"./chunk-UFVOKIMU.js";import"./chunk-VC37L5CU.js";import"./chunk-J624QXYK.js";export{a as UpcomingMoviesPageComponent};
---
+++ chunk-VJAKPVKO.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-2MSUXMSD.js";import"./chunk-J624QXYK.js";export{a as DetailsMoviePageComponent};
