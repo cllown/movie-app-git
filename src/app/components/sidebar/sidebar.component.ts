81c4dbc7a828665e4b537ed18cc49d4c00858ac1
cllown <artoorclownxd@gmail.com>
2025-05-11 22:06:12 +0300

    addedProgileButtonForProfilePage

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -2,14 +2,24 @@ import { Component } from '@angular/core';
 import { SidebarModule } from 'primeng/sidebar';
 import { ButtonModule } from 'primeng/button';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { selectIsLoggedIn } from '../../store/selectors';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs/internal/Observable';
+import * as MovieActions from '../../store/actions';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SidebarModule, ButtonModule, RouterLink],
+  imports: [SidebarModule, ButtonModule, RouterLink, CommonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
 export class SidebarComponent {
+  isLoggedIn$ = this.store.select(selectIsLoggedIn);
   sidebarVisible: boolean = false;
+  constructor(private store: Store) {}
+  openLoginPopup() {
+    this.store.dispatch(MovieActions.openLoginPopup());
+  }
 }
