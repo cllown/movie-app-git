b9f466f4f10c5c4c5546510597e8a0626a4cb9e9
cllown <artoorclownxd@gmail.com>
2025-05-16 13:18:27 +0300

    adaptiveFix

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -7,11 +7,18 @@ import { selectIsLoggedIn } from '../../store/selectors';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/internal/Observable';
 import * as MovieActions from '../../store/actions';
+import { MoviesSortingComponent } from '../movies-sorting/movies-sorting.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [SidebarModule, ButtonModule, RouterLink, CommonModule],
+  imports: [
+    SidebarModule,
+    ButtonModule,
+    RouterLink,
+    CommonModule,
+    MoviesSortingComponent,
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss',
 })
